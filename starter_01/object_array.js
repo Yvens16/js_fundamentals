8fb0ad7d5c29a0d243cddf1b7d6648e950ad2d26
Yvens16 <yvensbelaston@gmail.com>
2023-05-30 10:31:21 +0200

    go

---
+++ starter_01/object_array.js
@@ -0,0 +1,46 @@
+export function test () {
+  // Pour tester le tests
+  return 'hello';
+}
+
+export function objectValues () {
+  const obj = {}
+  return obj
+}
+
+export function objectToArrayKeys () {
+  const obj = {
+    name: 'Marie',
+    age: 23
+  }
+  return []
+}
+
+export function objectToArrayKeys2 () {
+  const obj = {
+    name: 'Marie',
+    age: 23
+  }
+  return []
+}
+
+export function pushInArray () {
+  const arr = [1, 2, 3]
+  return arr
+}
+
+export function objectToString () {
+  // Il faut utiliser les propriétés de l'objet ici et les template string
+  const obj = {
+    name: 'Marie',
+    age: 23
+  }
+  return "Je m'appelle [name] et j'ai [age] ans"
+}
+
+
+export function arrayToString () {
+  // Il faut utiliser les éléments du tableau ici et les template string
+  const arr = ["Marie", 23];
+  return "Je m'appelle [name] et j'ai [age] ans"
+}
---
+++ starter_01/object_array.test.js
@@ -0,0 +1,40 @@
+import {
+  test,
+  objectValues,
+  objectToArray,
+  objectToArrayKeys2,
+  pushInArray,
+  objectToString,
+  arrayToString
+} from './object_array'
+
+describe('First batch', () => {
+  it('Changez la valeur de renvoie de la fonction test dans le fichier pour voir le test passer au vert', () => {
+    expect(test()).toBe('bye')
+  })
+
+  it('Ça doit renvoyer un objet avec un name égal à Marie et un âge égal à 23', () => {
+    expect(objectValues()).toEqual(
+      expect.objectContaining({ name: 'Marie', age: 23 })
+    )
+  })
+  it('Ça doit renvoyer un tableau du type [clé, clé]', () => {
+    expect(objectToArray()).toEqual(['name', 'age'])
+  })
+
+  it('Ça doit renvoyer un tableau du type [clé, valeur, clé, valeur]', () => {
+    console.log('objectToArrayKeys2():', objectToArrayKeys2())
+    expect(objectToArrayKeys2()).toEqual(['name', 'Marie', 'age', 23])
+  })
+
+  it('Ça doit renvoyer le tableau [1,2,3,4]', () => {
+    expect(pushInArray()).toEqual([1, 2, 3, 4])
+  })
+  it('Ça doit renvoyer le tableau string compléter', () => {
+    expect(objectToString()).toEqual("Je m'appelle Marie et j'ai 23 ans")
+  })
+
+  it('Ça doit renvoyer le tableau string compléter avec les éléments du tableau', () => {
+    expect(arrayToString()).toEqual("Je m'appelle Marie et j'ai 23 ans")
+  })
+})
