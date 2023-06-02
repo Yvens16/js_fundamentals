85f4006de51211894d0a7d5af93deb23f127fe8c
bryce-sncf <bryce.marcol@sncf.fr>
2023-06-02 14:55:51 +0200

    problem OK

--- problems/problem.js
+++ problems/problem.js
@@ -1,11 +1,25 @@
-export function sumArray (array) {
-  return []
+export function sumArray(array) {
+  if (array.length < 1){
+    return []
+  }
+let result = [];
+for(let i = 0; i < array.length; i++){
+  i === 0
+  ? result.push(array[i])
+  : result.push(array[i] + array[i-1])
 }
+return result;
+} //////
+//pour cet exercice, j'ai tenté de modifier le test car il semblerait que la description ne corresponde pas au résultat attendu
+//voir les commentaires dans le test
+
 
 export function numberOfProperties (object) {
-  return []
+  return Object.keys(object).length;
 }
 
 export function stringReversal (string) {
-  return ''
+  return string.length < 1
+    ? '' 
+    : string.split('').reverse().join('');
 }
--- problems/problems.test.js
+++ problems/problems.test.js
@@ -2,8 +2,10 @@ import { sumArray, numberOfProperties, stringReversal } from './problem'
 
 describe('Des problèmes un peu plus compliqué', () => {
   it('Ça devrait prendre un tableau avec des chiffres et retourner un tableau dont chaque nombre est la somme de lui même + le chiffre avant lui', () => {
-    expect(sumArray([1, 2, 3, 4])).toStrictlyEqual([1, 4, 6, 8])
-  })
+    expect(sumArray([1, 2, 3, 4])).toStrictEqual([1, 3, 5, 7]) //j'ai modifié les valeurs car si je comprends bien la description, on devrait avoir 1, 3, 5, 7 et non 1, 4, 6, 8
+  })                                                              //[somme de 1 + 0, somme de 2 + 1, somme de 3 + 2, somme de 4 + 3]
+                                                                  //Et il semblerait que la méthode toStrictlyEqual n'existe pas, j'ai donc utilisé toStrictEqual (https://jestjs.io/fr/docs/expect#tostrictequalvalue)
+                                                                
   it("Ça devrait prendre un objet et compter le nombre de propriétés que l'objet possède", () => {
     const object = { name: 'Yvens', age: 23, surname: 'Belaston' }
     expect(numberOfProperties(object)).toBe(3)
