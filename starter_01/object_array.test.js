4b9d0edebe67df5dfc2b9d900fbc93caeb82ba3a
tawfik akaridan <tawfikear@gmail.com>
2023-06-02 23:56:30 +0200

    erreur de commit

--- starter_01/object_array.test.js
+++ starter_01/object_array.test.js
@@ -10,8 +10,8 @@ import {
 
 
 describe('First batch', () => {
-  it('Changez la valeur de renvoie de la fonction test dans le fichier pour voir le test passer au vert', () => {
-    expect(test()).toBe('hello')
+  it.only('Changez la valeur de renvoie de la fonction test dans le fichier pour voir le test passer au vert', () => {
+    expect(test()).toBe('bye')
   })
 
   it('Ça doit renvoyer un objet avec un name égal à Marie et un âge égal à 23', () => {
@@ -23,7 +23,7 @@ describe('First batch', () => {
     expect(objectToArrayKeys()).toEqual(['name', 'age'])
   })
 
-  it.only('Ça doit renvoyer un tableau du type [clé, valeur, clé, valeur]', () => {
+  it('Ça doit renvoyer un tableau du type [clé, valeur, clé, valeur]', () => {
     console.log('objectToArrayKeys2():', objectToArrayKeys2())
     expect(objectToArrayKeys2()).toEqual(['name', 'Marie', 'age', 23])
   })
@@ -31,7 +31,7 @@ describe('First batch', () => {
   it('Ça doit renvoyer le tableau [1,2,3,4]', () => {
     expect(pushInArray()).toEqual([1, 2, 3, 4])
   })
-  it.only('Ça doit renvoyer le tableau string compléter', () => {
+  it('Ça doit renvoyer le tableau string compléter', () => {
     expect(objectToString()).toEqual("Je m'appelle Marie et j'ai 23 ans")
   })
 
