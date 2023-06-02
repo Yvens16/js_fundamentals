5e8fad5e69915824c4c006e9e7890feee0806a06
moreaudamien <moreau_student2023@wilder.school>
2023-06-02 14:37:52 +0200

    exo3

--- problems/problem.js
+++ problems/problem.js
@@ -1,11 +1,42 @@
 export function sumArray (array) {
-  return []
+  function sumArray(arr) {
+    if (!Array.isArray(arr)) {
+      throw new Error("Le paramètre doit être un tableau.");
+    }
+  
+    if (arr.length < 2) {
+      return arr;
+    }
+  
+    const result = [];
+  
+    for (let i = 0; i < arr.length; i++) {
+      if (i === 0) {
+        result.push(arr[i]);
+      } else {
+        const sum = arr[i] + arr[i - 1];
+        result.push(sum);
+      }
+    }
+  
+    return result;
+  }
+
 }
 
 export function numberOfProperties (object) {
-  return []
+  if (typeof object !== "object" || object === null) {
+    throw new Error("Le paramètre doit être un objet.");
+  }
+
+  return Object.keys(object).length;
 }
+  
 
 export function stringReversal (string) {
-  return ''
+  if (typeof string !== "string") {
+    throw new Error("Le paramètre doit être une chaîne de caractères.");
+  }
+
+  return string.split("").reverse().join("");
 }
