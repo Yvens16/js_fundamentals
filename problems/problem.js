ad1b839a358f34a4f04aa0e149fb79f853f17f96
Krlito0 <cgnakine@gmail.com>
2023-06-02 17:08:37 +0200

    last practice

--- problems/problem.js
+++ problems/problem.js
@@ -1,11 +1,24 @@
 export function sumArray (array) {
-  return []
+    const newArray = [];
+for(let i=0; i<array.length; i++){
+  if(i===0){
+    newArray[i]=array[i]; 
+  }else{
+  newArray[i]=array[i]+array[i-1];
+  }
+}
+
+return newArray;
 }
 
 export function numberOfProperties (object) {
-  return []
+  
+return ((Object.keys(object)).length);
 }
 
 export function stringReversal (string) {
-  return ''
-}
+  const arrayString = [];
+  arrayString = string.split("");
+  return arrayString.reversed;
+} 
+/*impossible de lire le test lance que le premier*/
